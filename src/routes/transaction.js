f1150b0b7b7d2392d6e3f243c57fafd3d323af71
Jenny <jennifer.kannler@tum.de>
2022-06-13 17:59:31 +0200

    fixed audentification in transaction and user endpoints

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -7,8 +7,8 @@ import middlewares from '../middleware.js';
 import TransactionController from '../controllers/user'; //TODO: user mit transaction ersetzen!
 
 router.post('/', middlewares.checkAuthentication, TransactionController.create); // create new transaction
-router.get('/', TransactionController.list); // list all transactions
-router.get('/:id', middlewares.checkAuthentication, TransactionController.read); //read transaction by id
+router.get('/', middlewares.checkAuthentication, TransactionController.list); // list all transactions
+router.get('/:id:', middlewares.checkAuthentication, TransactionController.read); //read transaction by id
 router.put('/:id', middlewares.checkAuthentication, TransactionController.update); // update transaction by id
 router.delete('/:id', middlewares.checkAuthentication, TransactionController.remove); // delete transaction by id
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -6,8 +6,9 @@ import router from express.Router();
 import middlewares from '../middleware.js';
 import UserController from '../controllers/user';
 
-router.post('/', middlewares.checkAuthentication, UserController.create); // create new user
-router.get('/', UserController.list); // list all users
+router.post('/', UserController.create); // create new user
+//TODO middlewares.checkIsAdmin for list
+router.get('/', middlewares.checkAuthentication, UserController.list); // list all users 
 router.get('/:id', middlewares.checkAuthentication, UserController.read); //read user by id
 router.put('/:id', middlewares.checkAuthentication, UserController.update); // update user by id
 router.delete('/:id', middlewares.checkAuthentication, UserController.remove); // delete user by id
