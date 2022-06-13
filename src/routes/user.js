42bceb250945126871bc2ef2c4b8b202328ec9c8
Jenny <jennifer.kannler@tum.de>
2022-06-13 09:35:23 +0200

    add user endpoints

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,12 +6,26 @@ import router from express.Router();
 import middlewares from '../middleware.js';
 import UserController from '../controllers/user';
 
-// TODO: user endpoints for 
-// create new user
-// read all users
-// create, read, update, delete a user
-// create new transaction, read all transactions
-// crud transaction
+router.post('/', middlewares.checkAuthentication, UserController.create); // create new user
+router.get('/', UserController.list); // list all users
+router.get('/:id', middlewares.checkAuthentication, UserController.read); //read user by id
+router.put('/:id', middlewares.checkAuthentication, UserController.update); // update user by id
+router.delete('/:id', middlewares.checkAuthentication, UserController.remove); // delete user by id
+
+// TODO: user transaction endpoints for 
+
+// create new transaction
+// list all transactions
+// read transaction by id
+// update transaction by id
+// delete transaction by id
 
 
 module.exports = router;
+
+/*
+get: read-only
+post: creates a new resource
+put: update an existing resource
+delete: delete a resource specified by its URI
+ */
