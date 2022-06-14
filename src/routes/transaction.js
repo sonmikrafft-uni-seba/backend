ea4ed7ff8634a8f9158e0ac50551da1b03d579a7
Jenny <jennifer.kannler@tum.de>
2022-06-14 11:12:26 +0200

    fix transaction endpoint IDs

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -4,12 +4,12 @@ import express from express;
 import router from express.Router();
 
 import middlewares from '../middleware.js';
-import TransactionController from '../controllers/user'; //TODO: user mit transaction ersetzen!
+import TransactionController from '../controllers/user/transaction';
 
-router.post('/', middlewares.checkAuthentication, TransactionController.create); // create new transaction
-router.get('/', middlewares.checkAuthentication, TransactionController.list); // list all transactions
-router.get('/:id:', middlewares.checkAuthentication, TransactionController.read); //read transaction by id
-router.put('/:id', middlewares.checkAuthentication, TransactionController.update); // update transaction by id
-router.delete('/:id', middlewares.checkAuthentication, TransactionController.remove); // delete transaction by id
+router.post('/:id', middlewares.checkAuthentication, TransactionController.create); // create new transaction
+router.get('/:id', middlewares.checkAuthentication, TransactionController.list); // list all transactions
+router.get('/:id/:id', middlewares.checkAuthentication, TransactionController.read); //read transaction by id
+router.put('/:id/:id', middlewares.checkAuthentication, TransactionController.update); // update transaction by id
+router.delete('/:id/:id', middlewares.checkAuthentication, TransactionController.remove); // delete transaction by id
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -8,7 +8,7 @@ import UserController from '../controllers/user';
 
 router.post('/', UserController.create); // create new user
 //TODO middlewares.checkIsAdmin for list
-router.get('/', middlewares.checkAuthentication, UserController.list); // list all users 
+router.get('/:id', middlewares.checkAuthentication, UserController.list); // list all users 
 router.get('/:id', middlewares.checkAuthentication, UserController.read); //read user by id
 router.put('/:id', middlewares.checkAuthentication, UserController.update); // update user by id
 router.delete('/:id', middlewares.checkAuthentication, UserController.remove); // delete user by id
