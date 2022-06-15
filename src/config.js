a844399f1481a78a62ff16091f98afd51692ad54
Sonja Krafft <ga83bin@mytum.de>
2022-06-15 15:10:44 +0200

    set up docker for mongodb

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 // Configuration variables
 const port = process.env.PORT || '3001';
 const mongoURI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/budgetly';
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/budgetly'; //'mongodb://admin:secret@localhost:27017/'
 const jwtSecret = process.env.JWT_SECRET || 'budgetly';
 
 export default {
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -16,6 +16,16 @@ const remove = async (req, res) => {
 };
 
 const list  = async (req, res) => {
+    try {
+      let users = await UserModel.find({}).exec();
+    
+        return res.status(200).json(movies);
+      } catch(err) {
+        return res.status(500).json({
+          error: 'Internal server error',
+          message: err.message
+        });
+      }
 };
 
 
