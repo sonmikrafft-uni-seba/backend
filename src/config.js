a91883fd919f29d7c22eb31fa9eb9ce2e3cd0cf0
Tobias Vitt <tobias.vitt@tum.de>
2022-07-05 12:31:23 +0200

    Fix default bank

--- src/config.js
+++ src/config.js
@@ -11,5 +11,13 @@ const bankingSecret =
   'f71836026d78e72ab2d9778faef16088d420e17bfd848d95e9ea73ca779b85ce52a08e18c59bef30aa06374e32051c9113072faf4f61642a1cf8c7758619aab2';
 const bankingID =
   process.env.BANKING_ID || '06122eb5-39cc-407e-995b-5e4452bbba57';
-
-export { port, mongoURI, jwtSecret, jwtLifeTime, bankingID, bankingSecret };
+const frontendBaseUrl = 'http://localhost:3000';
+export {
+  port,
+  mongoURI,
+  jwtSecret,
+  jwtLifeTime,
+  bankingID,
+  bankingSecret,
+  frontendBaseUrl,
+};
