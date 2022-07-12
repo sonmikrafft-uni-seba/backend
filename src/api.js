e62981c26c96f946360c05baad939c6b56b4ec83
Tobias Vitt <tobias.vitt@tum.de>
2022-07-12 15:08:58 +0200

    extended by endpoints to add and update many transactions

--- src/api.js
+++ src/api.js
@@ -15,8 +15,8 @@ const api = express();
 
 // Adding Basic Middlewares
 api.use(helmet());
-api.use(bodyParser.json());
-api.use(bodyParser.urlencoded({ extended: false }));
+api.use(bodyParser.json({ limit: '50mb' }));
+api.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 api.use(middlewares.allowCrossDomain);
 
 // Basic route
