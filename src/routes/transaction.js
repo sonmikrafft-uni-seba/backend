ed7af4cc8a23aeb2c28507e4c704a941550c0416
Tobias Vitt <tobias.vitt@tum.de>
2022-07-29 01:34:15 +0200

    Added Authorization

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -9,36 +9,43 @@ import TransactionController from '../controllers/transaction.js';
 router.post(
   '/:userId/transaction/',
   middlewares.checkAuthentication,
+  middlewares.checkAuthorization,
   TransactionController.create
 ); // create new transaction
 router.get(
   '/:userId/transaction/',
   middlewares.checkAuthentication,
+  middlewares.checkAuthorization,
   TransactionController.list
 ); // list all transactions
 router.put(
   '/:userId/transaction/reassign',
   middlewares.checkAuthentication,
+  middlewares.checkAuthorization,
   TransactionController.updateMany
 ); // list all transactions
 router.post(
   '/:userId/transaction/many',
   middlewares.checkAuthentication,
+  middlewares.checkAuthorization,
   TransactionController.createMany
 ); // create new transaction
 router.get(
   '/:userId/transaction/:transactionId',
   middlewares.checkAuthentication,
+  middlewares.checkAuthorization,
   TransactionController.read
 ); //read transaction by id
 router.put(
   '/:userId/transaction/:transactionId',
   middlewares.checkAuthentication,
+  middlewares.checkAuthorization,
   TransactionController.update
 ); // update transaction by id
 router.delete(
   '/:userId/transaction/:transactionId',
   middlewares.checkAuthentication,
+  middlewares.checkAuthorization,
   TransactionController.remove
 ); // delete transaction by id
 
